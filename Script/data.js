84bf410d270b273228928648c2f926eb79b01d53
Tebogochonco <115028731+Tebogochonco@users.noreply.github.com>
2023-04-23 01:16:07 +0200

    updates
    working on the shop

---
+++ Script/data.js
@@ -0,0 +1,23 @@
+let shopItemsData = [
+    {
+      id: "box1",
+      name: "Party Pack Box 1",
+      price: 45,
+      desc: "party pack box",
+      img: "./Images/box1.jpg",
+    },
+    {
+        id: "box2",
+        name: "Party Pack Box 2",
+        price: 45,
+        desc: "party pack box",
+        img: "./Images/box2.jpg",
+      },
+      {
+        id: "box3",
+        name: "Party Pack Box 3",
+        price: 45,
+        desc: "party pack box",
+        img: "./Images/box3.jpg",
+      },
+]
